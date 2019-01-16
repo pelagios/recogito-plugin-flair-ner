afc66e51aa1d14fef618d4bc895b466c6018e9d9
Rainer Simon <rainer.simon@ait.ac.at>
2019-01-16 11:29:24 +0100

    Minor edits

--- src/test/scala/org/pelagios/recogito/plugins/ner/flair/FlairWrapperPluginSpec.scala
+++ src/test/scala/org/pelagios/recogito/plugins/ner/flair/FlairWrapperPluginSpec.scala
@@ -1,15 +1,30 @@
 package org.pelagios.recogito.plugins.ner.flair
 
+import java.io.File
 import org.specs2.mutable._
+import scala.io.Source
 
 class FlairWrapperPluginSpec extends Specification {
 
-  "The spec class" should {
+  import FlairWrapperPlugin._
 
-    "just work" in {
+  "The wrapper plugin" should {
+
+    "should parse the sample text" in {
+      val plugin = new FlairWrapperPlugin()
+
       1 must equalTo(1)
     }
 
+  }
+
+}
+
+object FlairWrapperPlugin {
+
+  lazy val SAMPLE_TEXT = {
+    val path = new File(getClass.getResource("/input.txt").getPath)
+    Source.fromFile(path).getLines.mkString("\n")
   }
 
 }
